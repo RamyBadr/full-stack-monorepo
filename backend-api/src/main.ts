420cc48342d1439d6dbeddd83663e814bc0c4a17
Ramy Badr <eng.ramybadr66@gmail.com>
2024-06-12 16:20:44 +0300

    feat(backend): add swagger
    Signed-off-by: Ramy Badr <eng.ramybadr66@gmail.com>

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -1,11 +1,6 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -13,6 +8,16 @@ async function bootstrap() {
   const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  const options = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('Nest js auth app')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('docs', app, document);
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
