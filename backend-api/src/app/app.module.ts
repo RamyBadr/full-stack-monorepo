030a1ae8676ae1dbf9a65fd493b577cb7476ae65
Ramy Badr <eng.ramybadr66@gmail.com>
2024-06-13 05:31:56 +0300

    feat(backend-users): init users resource, model
    Signed-off-by: Ramy Badr <eng.ramybadr66@gmail.com>

--- backend-api/src/app/app.module.ts
+++ backend-api/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from '../users/users.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/auth'),
+    MongooseModule.forRoot('mongodb://user:password@localhost:27017/project'),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
