1c25a40fceb8829e18feeba35ef679bd8521df11
Ramy Badr <eng.ramybadr66@gmail.com>
2024-06-13 07:15:39 +0300

    fix(backend): move env to root, unprotect root endpoint
    Signed-off-by: Ramy Badr <eng.ramybadr66@gmail.com>

--- backend-api/src/app/app.controller.ts
+++ backend-api/src/app/app.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from "@nestjs/common";
+import { Auth } from "../iam/authentication/decorators/auth.decorator";
+import { AuthType } from "../iam/authentication/enums/auth-type.enum";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
   }
 
+  @Auth(AuthType.None)
   @Get()
-  getData() {
+  healthcheck() {
     return this.appService.getData();
   }
 }
